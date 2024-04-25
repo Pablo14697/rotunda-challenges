c9806d3436299da14063bb0a24963a170f8f64f9
Pablo Emanuel Medina <pabloemanuelmedina@Pablos-Laptop.local>
2024-04-25 00:52:04 -0300

    feat: setting up repository and selector component 1st version is done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Selector } from "./components";
+
+function App() {
+  return (
+    <div className="flex flex-col h-screen w-screen items-center justify-center">
+      <Selector />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Selector/Selector.jsx
@@ -0,0 +1,59 @@
+// React
+import { useEffect, useRef, useState } from "react";
+
+// Constants
+import EMOJIS from "./constants";
+
+const DEFAULT_EMOJI_SELECTED_INDEX = 4;
+const EMOJI_SIZE_PX = 112;
+
+const Selector = () => {
+  const scroll = useRef(null);
+  const [selected, setSelected] = useState(DEFAULT_EMOJI_SELECTED_INDEX);
+  const [smoothScrollClass, setSmoothScrollClass] = useState("");
+
+  useEffect(() => {
+    if (scroll) {
+      scroll.current.scrollLeft += EMOJI_SIZE_PX;
+      setSmoothScrollClass("scroll-smooth");
+    }
+  }, []);
+
+  const onSelect = (index) => {
+    if (!EMOJIS[index]) return;
+    const multiplying = index > selected ? 1 : -1;
+
+    setSelected(index);
+    scroll.current.scrollLeft +=
+      multiplying * Math.abs((selected - index) * EMOJI_SIZE_PX);
+  };
+
+  return (
+    <div className="relative w-140 h-28 ">
+      <div
+        id="scroll"
+        ref={scroll}
+        className={`absolute w-full h-full flex items-center overflow-y-hidden  overflow-x-scroll ${smoothScrollClass} snap-x snap-mandatory`}
+      >
+        {EMOJIS.map((emoji, index) => (
+          <button
+            type="button"
+            onClick={() => onSelect(index)}
+            className="flex flex-column items-center justify-center snap-start min-h-28 min-w-28 max-h-28 max-w-28  outline-none"
+            key={index}
+            disabled={!emoji}
+          >
+            <span className="leading-[1.15] text-center text-[6rem] select-none">
+              {emoji}
+            </span>
+          </button>
+        ))}
+      </div>
+      <button
+        type="button"
+        className="absolute top-0 left-56 min-h-28 min-w-28 max-h-28 max-w-28 bg-[rgba(0,0,0,0.1)] border-[2px] border-[rgba(0,0,0,0.05)] rounded-3xl select-none"
+      />
+    </div>
+  );
+};
+export default Selector;
